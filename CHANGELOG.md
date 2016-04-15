979a641f4d4b51bdeecab5e2eb38a4fc3971aee1
Jan Lelis <mail@janlelis.de>
2016-04-15 21:32:19 +0200

    Fix for Ruby 2.2, 2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## CHANGELOG
 
+### 1.0.1
+
+* Fix for Ruby 2.2, 2.1
+
 ### 1.0.0
 
 * Inital release
--- lib/unicode/age/version.rb
+++ lib/unicode/age/version.rb
@@ -1,6 +1,6 @@
 module Unicode
   module Age
-    VERSION = "1.0.0".freeze
+    VERSION = "1.0.1".freeze
     UNICODE_VERSION = "8.0.0".freeze
   end
 end
