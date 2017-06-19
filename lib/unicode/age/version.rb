89839f52eb8918be44a34116dd46bb4fc8342f92
Jan Lelis <mail@janlelis.de>
2017-06-19 09:46:21 +0200

    Release v2.0.0

--- lib/unicode/age/version.rb
+++ lib/unicode/age/version.rb
@@ -1,7 +1,7 @@
 module Unicode
   module Age
-    VERSION = "2.0.0.pre".freeze
-    UNICODE_VERSION = "9.0.0".freeze
+    VERSION = "2.0.0".freeze
+    UNICODE_VERSION = "10.0.0".freeze
   end
 end
 
